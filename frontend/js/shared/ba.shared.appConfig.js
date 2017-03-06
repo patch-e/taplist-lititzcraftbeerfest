c12e467d670285138568f6f72cba7e26a00af078
patch-e <p.crager@gmail.com>
2017-03-05 21:31:07 -0500

    Untappd 3.0+ URL schema change

--- frontend/js/shared/ba.shared.appConfig.js
+++ frontend/js/shared/ba.shared.appConfig.js
@@ -11,7 +11,7 @@ Patrick Crager
 
   angular.module('beersApp.shared').constant('appConfig', {
 
-    BA_UNTAPPD_APP_SCHEME: 'untappd:///?beer=',
+    BA_UNTAPPD_APP_SCHEME: 'untappd://beer/',
     BA_UNTAPPD_URL_SCHEME: 'http://untappd.com/beer/',
     BA_UNTAPPD_CLIENTID: '212A5A8EA9330E5873C9C057FD5941598A54F1CE',
     BA_UNTAPPD_LOGIN_URL: 'https://untappd.com/oauth/authenticate/?client_id={0}&response_type=token&redirect_url={1}',
