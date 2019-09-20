a2da6d8617f5962d3964ac97911e537be716873a
Patrick Crager <p.crager@gmail.com>
2019-09-19 21:38:40 -0400

    2019 beer list update

--- frontend/release/js/beersapp.js
+++ frontend/release/js/beersapp.js
@@ -514,7 +514,7 @@ Patrick Crager
     return factory;
 
     function list() {
-      return $http.get('/data/taplist-20190917.json');
+      return $http.get('/data/taplist-20190919.json');
     }
 
     function search(b, n) {
