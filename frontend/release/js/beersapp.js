68304a98e2668068b6a94f0c751130a99d962c77
Patrick Crager <p.crager@gmail.com>
2021-09-19 10:09:55 -0400

    Prep codebase for 2021 fest

--- frontend/release/js/beersapp.js
+++ frontend/release/js/beersapp.js
@@ -379,10 +379,9 @@ Patrick Crager
     'beersApp.controllers'
   ]).config(configure);
 
-  configure.$inject = ['$locationProvider', '$routeProvider'];
+  configure.$inject = ['$routeProvider'];
 
-  function configure($locationProvider, $routeProvider) {
-    $locationProvider.hashPrefix('!'); 
+  function configure($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'partials/table.html',
@@ -442,8 +441,8 @@ Patrick Crager
     BA_UNTAPPD_APP_SCHEME: 'untappd://beer/',
     BA_UNTAPPD_URL_SCHEME: 'http://untappd.com/beer/',
     BA_UNTAPPD_CLIENTID: '212A5A8EA9330E5873C9C057FD5941598A54F1CE',
-    BA_UNTAPPD_LOGIN_URL: 'https://untappd.com/oauth/authenticate/?client_id={0}&response_type=token&redirect_url={1}',
-    BA_UNTAPPD_CALLBACK_URL: 'http://taplist.lititzcraftbeerfest.com',
+    BA_UNTAPPD_LOGIN_URL: 'https://untappd.com/oauth/authenticate/?client_id={0}&response_type=code&redirect_url={1}',
+    BA_UNTAPPD_CALLBACK_URL: 'http://taplist.lititzcraftbeerfest.com/nodejs/beer/login',
     BA_LOGOUT_URL: '/nodejs/beer/logout'
 
   });
