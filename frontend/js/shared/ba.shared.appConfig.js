80d5201b86a220b0a087b8a611eade4bdd6187df
Patrick Crager <p.crager@gmail.com>
2021-09-19 12:10:21 -0400

    Clean up yearly files, put app into coming soon mode

--- frontend/js/shared/ba.shared.appConfig.js
+++ frontend/js/shared/ba.shared.appConfig.js
@@ -15,7 +15,7 @@ Patrick Crager
     BA_UNTAPPD_URL_SCHEME: 'http://untappd.com/beer/',
     BA_UNTAPPD_CLIENTID: '212A5A8EA9330E5873C9C057FD5941598A54F1CE',
     BA_UNTAPPD_LOGIN_URL: 'https://untappd.com/oauth/authenticate/?client_id={0}&response_type=code&redirect_url={1}',
-    BA_UNTAPPD_CALLBACK_URL: 'http://taplist.lititzcraftbeerfest.com/nodejs/beer/login',
+    BA_UNTAPPD_CALLBACK_URL: 'https://taplist.lititzcraftbeerfest.com/nodejs/beer/login',
     BA_LOGOUT_URL: '/nodejs/beer/logout'
 
   });
